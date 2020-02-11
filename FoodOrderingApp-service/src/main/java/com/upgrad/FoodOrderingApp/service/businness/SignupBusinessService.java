fa95f4f2b85cfcf1507bc0e5405886de175e2db2
Dipesh Singhal <dipesh.singhal@gmail.com>
2020-02-11 09:53:19 +0530

    Adding customer signup service and customer Entity

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignupBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignupBusinessService.java
@@ -1,7 +1,31 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SignupBusinessService {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public CustomerEntity signup(CustomerEntity customerEntity) throws SignUpRestrictedException {
+        System.out.println("SignupBusinessService..." + customerEntity);
+
+
+        //TODO: pre check before persisting customer Entity
+
+        try {
+            return customerDao.createCustomer(customerEntity);
+        } catch (Exception e) {
+            throw new SignUpRestrictedException("SGR-000","Unknown database error while creating customer");
+        }
+
+    }
 }
